194b954286a0b1bd2bb49d73f2934ef84e610859
donezio <donezio32@gmail.com>
2017-11-27 15:21:11 -0800

    dasd

--- Tapit/GameScene.swift
+++ Tapit/GameScene.swift
@@ -9,81 +9,68 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene , UIGestureRecognizerDelegate{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let height = UIScreen.main.bounds.size.height
+    let width  = UIScreen.main.bounds.size.width
     
     override func didMove(to view: SKView) {
+        self.view?.isMultipleTouchEnabled = true;
+        //setting background color
+        backgroundColor = SKColor.white
+        let area1 = SKShapeNode(rectOf: CGSize(width: width, height: height/2))
+        area1.fillColor = SKColor.black
+        let area2 = SKShapeNode(rectOf: CGSize(width: width, height: height/2))
+        area2.fillColor = SKColor.gray
+        // (0 ,0) is at the left botton
+        // position is the center of the object
+        area1.position = CGPoint(x: width/2, y: height/4)
+        area2.position = CGPoint(x: width/2, y: 3 * height/4)
+        area1.name = "area1"
+        area2.name = "area2"
+       
+        area1.isUserInteractionEnabled = false
+        area2.isUserInteractionEnabled = false
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        addChild(area1)
+        addChild(area2)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+    /**
+        function to detect touch begin
+     */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        print("i am called")
+        let touch:UITouch = touches.first!
+        let positionInScene = touch.location(in: self)
+        let touchedNode = self.atPoint(positionInScene)
+        if let name = touchedNode.name
+        {
+            if name == "area1"
+            {
+                print("area1")
+            }
+            if name == "area2"{
+                print("area2")
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+    /**
+        function to detect touch end
+    */
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let touch:UITouch = touches.first!
+        let positionInScene = touch.location(in: self)
+        let touchedNode = self.atPoint(positionInScene)
+        if let name = touchedNode.name
+        {
+            if name == "area1"
+            {
+                print("end touching area1")
+            }
+            if name == "area2"{
+                print("end touch area2")
+            }
+        }
     }
 }
